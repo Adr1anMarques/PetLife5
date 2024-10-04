9487b2c66fa574a7d37f0f8d5afdc64540687e6c
Adr1anMarques <adriansantanarey@gmail.com>
2024-10-04 16:37:53 -0300

    start commit

---
+++ app/src/main/java/com/example/login/FormCadastroFeito.java
@@ -0,0 +1,30 @@
+package com.example.login;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class FormCadastroFeito extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_form_cadastro_feito);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    public void telaLogin (View o){
+        Intent intent = new Intent(FormCadastroFeito.this,FormLogin.class);
+        startActivity(intent);
+    }
+}
