ed6ba3ea803bc8f15bd348f0a1d0841bd77a83d3
Adr1anMarques <adriansantanarey@gmail.com>
2024-11-13 15:36:01 -0300

    start activi

--- app/src/main/java/com/example/login/FormInformacoes.java
+++ app/src/main/java/com/example/login/FormInformacoes.java
@@ -23,10 +23,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class FormInformacoes extends AppCompatActivity {
-
-    private EditText edit_raca, edit_idade, edit_saude;
     private Button bt_salvar;
-
+    private EditText edit_raca, edit_idade, edit_saude;
+    String usuarioID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,16 +38,45 @@ public class FormInformacoes extends AppCompatActivity {
             return insets;
         });
         iniciarComponentes();
-
-
+        bt_salvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SalvarDados();
+            }
+        });
     }
-    private void iniciarComponentes(){
+    public void SalvarDados(){
+        String raca = edit_raca.getText().toString();
+        String idade = edit_idade.getText().toString();
+        String saude = edit_saude.getText().toString();
+
+        FirebaseFirestore banco = FirebaseFirestore.getInstance();
+
+        Map<String,Object> usuarios = new HashMap<>();
+        usuarios.put("raça",raca);
+        usuarios.put("idade",idade);
+        usuarios.put("saude",saude);
+
+        usuarioID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DocumentReference documentReference = banco.collection("PETS").document(usuarioID);
+        documentReference.set(usuarios).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Log.d("banco","Dados salvos com SUCESSO");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("banco_error", "Erro ao Salvar"+ e.toString());
+                    }
+                });
+    }
+    public void iniciarComponentes(){
+        bt_salvar= findViewById(R.id.bt_infPet);
         edit_raca = findViewById(R.id.edit_raca);
         edit_idade = findViewById(R.id.edit_idade);
         edit_saude = findViewById(R.id.edit_saude);
-        bt_salvar = findViewById(R.id.button_informacao);
     }
-
-
-    }
+}
 
--- app/src/main/java/com/example/login/FormTosa.java
+++ app/src/main/java/com/example/login/FormTosa.java
@@ -1,14 +1,29 @@
 package com.example.login;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class FormTosa extends AppCompatActivity {
+   private Button bt_salvar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +35,40 @@ public class FormTosa extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        iniciarComponentes();
+        bt_salvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SalvarDados();
+            }
+        });
+    }
+
+
+    public void SalvarDados(){
+        String nome = "amem";
+
+        FirebaseFirestore banco = FirebaseFirestore.getInstance();
+
+        Map<String,Object> usuarios = new HashMap<>();
+        usuarios.put("nome",nome);
+
+
+        DocumentReference documentReference = banco.collection("PET").document("GRAÇAS");
+        documentReference.set(usuarios).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Log.d("banco","Dados salvos com SUCESSO");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("banco_error", "Erro ao Salvar"+ e.toString());
+                    }
+                });
+    }
+    public void iniciarComponentes(){
+        bt_salvar= findViewById(R.id.bt_escolher);
     }
 }
